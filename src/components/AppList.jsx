5906d61c71fceb87fb41558d1dd895ed3338fd35
local_ubuntu <kanikama775@gmail.com>
2023-09-15 01:43:35 +0900

    :+1: スマホ用 Apps静的ページ作成

---
+++ src/components/AppList.jsx
@@ -0,0 +1,48 @@
+import { Box } from "@chakra-ui/react";
+import { AppCard } from "./AppCard";
+
+export const AppList = () => {
+  const AppSet = [
+    {
+      AppName: "App1",
+      AppImagePath:
+        "https://3.bp.blogspot.com/-mpUpAHyCx8M/UpGGhQrwAkI/AAAAAAAAa6o/WFgepnnzR98/s400/bird_kakkou.png",
+      AppDescription: "Desc1",
+      AppLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
+    },
+    {
+      AppName: "App2",
+      AppImagePath:
+        "https://4.bp.blogspot.com/-hBEnxhO8fjc/W8BOGryIf2I/AAAAAAABPVs/TWoScpc8jUonypEy1Fj01n-PrC-9gI5gACLcBGAs/s400/bird_kojukei.png",
+      AppDescription: "Desc2",
+      AppLink: "https://www.irasutoya.com/2018/11/blog-post_68.html",
+    },
+    {
+      AppName: "App3",
+      AppImagePath:
+        "https://1.bp.blogspot.com/-nMTqgygyczs/VqtZItHn2_I/AAAAAAAA3ZY/5ZfrZPuZzbY/s450/bird_aoitori_bluebird.png",
+      AppDescription: "Desc3",
+      AppLink: "https://www.irasutoya.com/2013/12/blog-post_7916.html",
+    },
+  ];
+  return (
+    <>
+      <Box
+        bg={"green.300"}
+        padding={"3vh"}
+        display={"flex"}
+        flexFlow={"column"}
+        alignItems={"center"}
+      >
+        {AppSet.map((App) => (
+          <AppCard
+            Name={App.AppName}
+            AppImage={App.AppImagePath}
+            Description={App.AppDescription}
+            Links={App.AppLink}
+          ></AppCard>
+        ))}
+      </Box>
+    </>
+  );
+};
