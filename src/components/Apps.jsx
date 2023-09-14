9d269e8308f2c924367ada0533c7dcb365b43981
Lamaglama39 <lamaglama621@gmail.com>
2023-09-14 23:12:40 +0900

    :sparkles: React Routerでページ移動を仮追加

---
+++ src/components/Apps.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { Box } from "@chakra-ui/react";
+import "../App.css";
+
+import { TopBar } from "./TopBar";
+
+function Apps() {
+  return (
+    <>
+      <TopBar></TopBar>
+      <h1>Apps!!!</h1>
+    </>
+  );
+}
+
+export default Apps;
