d28a8f07c5f5901422b223c2685be083fae6aa1f
local_ubuntu <kanikama775@gmail.com>
2023-09-30 14:22:29 +0900

    :recycle: TopPageのスタイリング微調整

--- src/components/VicugnaModel.jsx
+++ src/components/VicugnaModel.jsx
@@ -2,13 +2,13 @@ import { Image } from "@chakra-ui/react";
 
 export const VicugnaModel = () => {
   return (
-    <div>
+    <>
       <Image
         width={"45vh"}
         height={"45vh"}
         src="https://1.bp.blogspot.com/-PNtqP_kYbEw/XuMM_ImLmVI/AAAAAAABZeo/7aJRwvd66KEEIsNzjP3ddRxz_DsAWJlZQCNcBGAsYHQ/s400/animal_alpaca_huacaya.png"
         alt="Vicugna-3D-Model"
       />
-    </div>
+    </>
   );
 };
