a641c2b599a391a0197612c97c2ea25f42d17509
Lamaglama39 <lamaglama621@gmail.com>
2023-10-02 01:17:34 +0900

    :recycle: AppsPageのスタイリング           AppCardのサンプルデータ追加

--- src/utils/AppInfoList.jsx
+++ src/utils/AppInfoList.jsx
@@ -1,44 +1,56 @@
 export const AppInfoList = [
   {
-    AppName: "App1",
+    AppName: "Lamaglama39のポートフォリオ",
     AppImagePath:
       "https://3.bp.blogspot.com/-mpUpAHyCx8M/UpGGhQrwAkI/AAAAAAAAa6o/WFgepnnzR98/s400/bird_kakkou.png",
-    AppDescription: "Desc1",
+    AppDescription:
+      "今見ているポートフォリオです。\nこだわりポイントはなんて言ってもチャーミングなアルパカちゃんです!\nくるくる回して遊んでみてね。",
     AppLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
+    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
   },
   {
-    AppName: "App2",
+    AppName: "無限ぷちぷち",
     AppImagePath:
       "https://4.bp.blogspot.com/-hBEnxhO8fjc/W8BOGryIf2I/AAAAAAABPVs/TWoScpc8jUonypEy1Fj01n-PrC-9gI5gACLcBGAs/s400/bird_kojukei.png",
-    AppDescription: "Desc2",
+    AppDescription:
+      "懐かしの無限ぷちぷち...。\nあの頃には出来なかった真の無限ぷちぷちをご堪能ください。",
     AppLink: "https://www.irasutoya.com/2018/11/blog-post_68.html",
+    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
   },
   {
-    AppName: "App3",
+    AppName: "タイピング・パラダイス",
     AppImagePath:
       "https://1.bp.blogspot.com/-nMTqgygyczs/VqtZItHn2_I/AAAAAAAA3ZY/5ZfrZPuZzbY/s450/bird_aoitori_bluebird.png",
-    AppDescription: "Desc3",
+    AppDescription:
+      "四方から襲い掛かるカオスなタイピングゲームを目指して作りました。\n一味違ったタイピング体験ができます。",
     AppLink: "https://www.irasutoya.com/2013/12/blog-post_7916.html",
+    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
   },
   {
-    AppName: "App1",
+    AppName: "ChatGPT 組み込みライブラリ",
     AppImagePath:
-      "https://3.bp.blogspot.com/-mpUpAHyCx8M/UpGGhQrwAkI/AAAAAAAAa6o/WFgepnnzR98/s400/bird_kakkou.png",
-    AppDescription: "Desc1",
+      "https://1.bp.blogspot.com/-nMTqgygyczs/VqtZItHn2_I/AAAAAAAA3ZY/5ZfrZPuZzbY/s450/bird_aoitori_bluebird.png",
+    AppDescription:
+      "ChatGPTを気軽にReactに組み込むためのライブラリです。\nこのポートフォリオのアルパカちゃんにも実装しています。\nあと意外と高いGPT4の使用料金にはお気をつけください...。",
     AppLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
+    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
   },
   {
-    AppName: "App2",
+    AppName: "ローポリ動物図鑑",
     AppImagePath:
       "https://4.bp.blogspot.com/-hBEnxhO8fjc/W8BOGryIf2I/AAAAAAABPVs/TWoScpc8jUonypEy1Fj01n-PrC-9gI5gACLcBGAs/s400/bird_kojukei.png",
-    AppDescription: "Desc2",
+    AppDescription:
+      "低解像度の動物しか得られない栄養素があると信じて、あえてローポリで作成したオンライン動物図鑑です。\nサイト内の動物モデルは無料配布しているので、利用規約を守って使ってみてね。",
     AppLink: "https://www.irasutoya.com/2018/11/blog-post_68.html",
+    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
   },
   {
-    AppName: "App3",
+    AppName: "Identiconインベーダー",
     AppImagePath:
       "https://1.bp.blogspot.com/-nMTqgygyczs/VqtZItHn2_I/AAAAAAAA3ZY/5ZfrZPuZzbY/s450/bird_aoitori_bluebird.png",
-    AppDescription: "Desc3",
+    AppDescription:
+      "Githubのあのアイコンでプレイ出来るインベーダーゲームです。\nちなみに「アイデンティコン」て読むらしいです、私はこのアプリ作るまで知りませんでした...。",
     AppLink: "https://www.irasutoya.com/2013/12/blog-post_7916.html",
+    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
   },
 ];
