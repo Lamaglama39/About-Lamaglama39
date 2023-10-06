3bcbf8921e1d501b651ccf32fde709c4ae905dea
local_ubuntu <kanikama775@gmail.com>
2023-10-06 23:27:01 +0900

    :+1: スクロールボタンを追加

--- src/components/VicugnaArea.jsx
+++ src/components/VicugnaArea.jsx
@@ -1,20 +1,31 @@
-import { Box } from "@chakra-ui/react";
-import { TitleText } from "./TitleText";
-import { VicugnaModel } from "./VicugnaModel";
+import { Box, Text, useMediaQuery } from "@chakra-ui/react";
+import VicugnaModel from "./VicugnaModel";
+import { ScrollBottomButton } from "../components/ScrollBottomButton";
 
 export const VicugnaArea = () => {
+  // Check the current viewport width.
+  const [isLargerThanMD] = useMediaQuery("(min-width: 48em)"); // Adjust the value as per your requirement.
+
   return (
     <>
       <Box
-        marginTop={["5vh"]}
-        width={["100%", "100%", "70%"]}
+        width={["100%", "100%", "75%"]}
         textAlign={"center"}
+        position={"relative"}
       >
-        <TitleText
-          title={"Lamaglama39"}
-          size={["24px", "32px", "40px"]}
-        ></TitleText>
+        <Text
+          fontSize={["32px", "40px", "48px"]}
+          fontWeight={"bold"}
+          position={"absolute"}
+          top={["8vh"]}
+          left={["50%"]}
+          transform={["translate(-50%, -50%)"]}
+        >
+          I&apos;am <br /> Lamaglama39...
+        </Text>
         <VicugnaModel></VicugnaModel>
+        {/* Display ScrollBottomButton only when the viewport width is less than the md breakpoint */}
+        {!isLargerThanMD && <ScrollBottomButton text={"🦙What Skills?🦙"} />}
       </Box>
     </>
   );
