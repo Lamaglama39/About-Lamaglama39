292dcd8932ab9512a38bf2d76c51c95a96fc62f6
Lamaglama39 <lamaglama621@gmail.com>
2023-10-13 22:15:31 +0900

    :+1: スクロール用ボタンコンポーネントを追加

--- src/components/ScrollBottomButton.jsx
+++ src/components/ScrollBottomButton.jsx
@@ -1,5 +1,6 @@
 import { Button } from "@chakra-ui/react";
 import { animateScroll as scroll } from "react-scroll";
+import PropTypes from "prop-types";
 
 export const ScrollBottomButton = ({ text }) => {
   return (
@@ -16,4 +17,8 @@ export const ScrollBottomButton = ({ text }) => {
       {text}
     </Button>
   );
+};
+
+ScrollBottomButton.propTypes = {
+  text: PropTypes.string.isRequired,
 };
---
+++ src/components/ScrollTargetButton.jsx
@@ -0,0 +1,37 @@
+import { Button } from "@chakra-ui/react";
+import { animateScroll as scroll } from "react-scroll";
+import PropTypes from "prop-types";
+
+export const ScrollTargetButton = ({ text, target, offsets, styleProps }) => {
+  const scrollToTarget = () => {
+    const element = document.querySelector(target);
+    if (element) {
+      const offsetSpaceVh = offsets;
+      const offsetSpaceInPx = (window.innerHeight * offsetSpaceVh) / 100; // vhをpxに変換
+
+      // エレメントの位置までスクロール（余分なスペースを考慮してオフセットを減算）
+      const offset = element.offsetTop - offsetSpaceInPx;
+      scroll.scrollTo(offset, { duration: 1000, smooth: true });
+    }
+  };
+
+  return (
+    <Button
+      bg={"none"}
+      border={"none"}
+      padding={"1em"}
+      fontSize={["2em", "2em", "2.5em"]}
+      {...styleProps}
+      onClick={scrollToTarget}
+    >
+      {text}
+    </Button>
+  );
+};
+
+ScrollTargetButton.propTypes = {
+  text: PropTypes.string.isRequired,
+  target: PropTypes.string.isRequired,
+  offsets: PropTypes.string.isRequired,
+  styleProps: PropTypes.object.isRequired,
+};
