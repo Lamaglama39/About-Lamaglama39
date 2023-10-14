3a0b6c907d3c67faf72eb7ca8ff86bbc0211fc87
Lamaglama39 <lamaglama621@gmail.com>
2023-10-14 14:41:12 +0900

    :lipstick: ModeIconのサイズ/位置調整

--- src/components/ModeIcon.jsx
+++ src/components/ModeIcon.jsx
@@ -4,11 +4,19 @@ import { MoonIcon, SunIcon } from "@chakra-ui/icons";
 export const ModeIcon = () => {
   const { colorMode, toggleColorMode } = useColorMode();
   return (
-    <Button onClick={toggleColorMode} w={"1%"} border={"none"} bg={"none"}>
+    <Button
+      onClick={toggleColorMode}
+      w={"2em"}
+      border={"none"}
+      bg={"none"}
+      position={"fixed"}
+      top="4em"
+      right={"1em"}
+    >
       {colorMode === "light" ? (
-        <MoonIcon fontSize={"3vh"} />
+        <MoonIcon fontSize={"2em"} />
       ) : (
-        <SunIcon fontSize={"3vh"} />
+        <SunIcon fontSize={"2em"} />
       )}
     </Button>
   );
