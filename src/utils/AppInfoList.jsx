eaa99df13ef599b0ab60554712a68074ff77f221
Lamaglama39 <lamaglama621@gmail.com>
2023-10-18 23:51:59 +0900

    :truck: png/glbファイルをpublicフォルダへ移動

--- src/utils/AppInfoList.jsx
+++ src/utils/AppInfoList.jsx
@@ -1,7 +1,7 @@
 export const AppInfoList = [
   {
     AppName: "About Lamaglama39",
-    AppImagePath: "src/assets/png/about-lamaglama39-info.png",
+    AppImagePath: "/assets/png/about-lamaglama39-info.png",
     AppDescription:
       "今見ているポートフォリオです。\n無心でラマをくるくる回して遊んでみてね。",
     AppLink: "https://Lamaglama39.github.io/About-Lamaglama39/",
@@ -9,7 +9,7 @@ export const AppInfoList = [
   },
   {
     AppName: "無限ぷちぷち",
-    AppImagePath: "src/assets/png/infinite-puchi-info.png",
+    AppImagePath: "/assets/png/infinite-puchi-info.png",
     AppDescription:
       "懐かしの無限ぷちぷち...。\nあの頃には出来なかった真の無限ぷちぷちをご堪能ください。",
     AppLink: "https://d3a5y9l67jnuw.cloudfront.net/",
@@ -17,7 +17,7 @@ export const AppInfoList = [
   },
   {
     AppName: "タイピング・パラダイス",
-    AppImagePath: "src/assets/png/typing-paradise-info.png",
+    AppImagePath: "/assets/png/typing-paradise-info.png",
     AppDescription:
       "四方から文字が襲い掛かるカオスなタイピングゲームです。\n一味違ったタイピング体験ができます。",
     AppLink: "https://d3n4v8721djrpd.cloudfront.net/",
