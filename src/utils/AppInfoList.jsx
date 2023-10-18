08ca0fc61454e2e66e493deda05c79b4373b356f
local_ubuntu <kanikama775@gmail.com>
2023-10-18 22:45:21 +0900

    :lipstick: IconSetのサイズ調整/レスポンシブ調整 :+1: AppInfoListのアプリ説明分の修正

--- src/utils/AppInfoList.jsx
+++ src/utils/AppInfoList.jsx
@@ -1,56 +1,26 @@
 export const AppInfoList = [
   {
-    AppName: "Lamaglama39のポートフォリオ",
-    AppImagePath:
-      "https://3.bp.blogspot.com/-mpUpAHyCx8M/UpGGhQrwAkI/AAAAAAAAa6o/WFgepnnzR98/s400/bird_kakkou.png",
+    AppName: "About Lamaglama39",
+    AppImagePath: "src/assets/png/about-lamaglama39-info.png",
     AppDescription:
-      "今見ているポートフォリオです。\nこだわりポイントはなんて言ってもチャーミングなアルパカちゃんです!\nくるくる回して遊んでみてね。",
-    AppLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
+      "今見ているポートフォリオです。\n無心でラマをくるくる回して遊んでみてね。",
+    AppLink: "https://Lamaglama39.github.io/About-Lamaglama39/",
     GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
   },
   {
     AppName: "無限ぷちぷち",
-    AppImagePath:
-      "https://4.bp.blogspot.com/-hBEnxhO8fjc/W8BOGryIf2I/AAAAAAABPVs/TWoScpc8jUonypEy1Fj01n-PrC-9gI5gACLcBGAs/s400/bird_kojukei.png",
+    AppImagePath: "src/assets/png/infinite-puchi-info.png",
     AppDescription:
       "懐かしの無限ぷちぷち...。\nあの頃には出来なかった真の無限ぷちぷちをご堪能ください。",
-    AppLink: "https://www.irasutoya.com/2018/11/blog-post_68.html",
-    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
+    AppLink: "https://d3a5y9l67jnuw.cloudfront.net/",
+    GithubLink: "https://github.com/Lamaglama39/bubblewrap-paradise",
   },
   {
     AppName: "タイピング・パラダイス",
-    AppImagePath:
-      "https://1.bp.blogspot.com/-nMTqgygyczs/VqtZItHn2_I/AAAAAAAA3ZY/5ZfrZPuZzbY/s450/bird_aoitori_bluebird.png",
+    AppImagePath: "src/assets/png/typing-paradise-info.png",
     AppDescription:
-      "四方から襲い掛かるカオスなタイピングゲームを目指して作りました。\n一味違ったタイピング体験ができます。",
-    AppLink: "https://www.irasutoya.com/2013/12/blog-post_7916.html",
-    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
-  },
-  {
-    AppName: "ChatGPT 組み込みライブラリ",
-    AppImagePath:
-      "https://1.bp.blogspot.com/-nMTqgygyczs/VqtZItHn2_I/AAAAAAAA3ZY/5ZfrZPuZzbY/s450/bird_aoitori_bluebird.png",
-    AppDescription:
-      "ChatGPTを気軽にReactに組み込むためのライブラリです。\nこのポートフォリオのアルパカちゃんにも実装しています。\nあと意外と高いGPT4の使用料金にはお気をつけください...。",
-    AppLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
-    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
-  },
-  {
-    AppName: "ローポリ動物図鑑",
-    AppImagePath:
-      "https://4.bp.blogspot.com/-hBEnxhO8fjc/W8BOGryIf2I/AAAAAAABPVs/TWoScpc8jUonypEy1Fj01n-PrC-9gI5gACLcBGAs/s400/bird_kojukei.png",
-    AppDescription:
-      "低解像度の動物しか得られない栄養素があると信じて、あえてローポリで作成したオンライン動物図鑑です。\nサイト内の動物モデルは無料配布しているので、利用規約を守って使ってみてね。",
-    AppLink: "https://www.irasutoya.com/2018/11/blog-post_68.html",
-    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
-  },
-  {
-    AppName: "Identiconインベーダー",
-    AppImagePath:
-      "https://1.bp.blogspot.com/-nMTqgygyczs/VqtZItHn2_I/AAAAAAAA3ZY/5ZfrZPuZzbY/s450/bird_aoitori_bluebird.png",
-    AppDescription:
-      "Githubのあのアイコンでプレイ出来るインベーダーゲームです。\nちなみに「アイデンティコン」て読むらしいです、私はこのアプリ作るまで知りませんでした...。",
-    AppLink: "https://www.irasutoya.com/2013/12/blog-post_7916.html",
-    GithubLink: "https://www.irasutoya.com/2013/12/blog-post_826.html",
+      "四方から文字が襲い掛かるカオスなタイピングゲームです。\n一味違ったタイピング体験ができます。",
+    AppLink: "https://d3n4v8721djrpd.cloudfront.net/",
+    GithubLink: "https://github.com/Lamaglama39/typing-paradise",
   },
 ];
