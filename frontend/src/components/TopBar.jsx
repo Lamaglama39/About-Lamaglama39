f80b7a022301609a412883be5965b7af92520c2a
Lamaglama39 <lamaglama621@gmail.com>
2023-12-09 23:10:29 +0900

    :lipstick: TopBarにTopDrawerを追加、全体的なスタイリングを修正

--- frontend/src/components/TopBar.jsx
+++ frontend/src/components/TopBar.jsx
@@ -1,25 +1,29 @@
 import { Box } from "@chakra-ui/react";
 import { TopButtonList } from "./TopButtonList";
+import { TopDrawer } from "./TopDrawer";
+
 import { ModeIcon } from "./ModeIcon";
 import PropTypes from "prop-types";
 
 export const TopBar = ({ onNavigate }) => {
   return (
-    <div>
-      <Box
-        width={"100%"}
-        height={"3em"}
-        display={"flex"}
-        justifyContent={"space-between"}
-        alignItems={"center"}
-        position={"fixed"}
-        zIndex="docked"
-        className="TopBar"
-      >
-        <TopButtonList onNavigate={onNavigate}></TopButtonList>
-        <ModeIcon light={false}></ModeIcon>
-      </Box>
-    </div>
+    <Box
+      width={"100%"}
+      height={"3em"}
+      display={"flex"}
+      justifyContent={"start"}
+      position={"fixed"}
+      zIndex="docked"
+      className="TopBar"
+    >
+      <TopButtonList
+        onNavigate={onNavigate}
+        display={["none", "none", "flex"]}
+      ></TopButtonList>
+      <TopDrawer display={["flex", "flex", "none"]}></TopDrawer>
+
+      <ModeIcon light={false}></ModeIcon>
+    </Box>
   );
 };
 
