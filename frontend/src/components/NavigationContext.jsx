fcf9b29102e852f6adbeafe4bad697ccabf36ee6
Lamaglama39 <lamaglama621@gmail.com>
2025-05-04 17:21:37 +0900

    :fire: remove old version

--- frontend/src/components/NavigationContext.jsx
+++
@@ -1,9 +0,0 @@
-import { createContext } from "react";
-
-const NavigationContext = createContext({
-  navigate: () => {},
-  inProp: false,
-  setInProp: () => {},
-});
-
-export { NavigationContext };
